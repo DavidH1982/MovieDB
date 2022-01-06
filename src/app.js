8be40a69c2e241457c84fed735ede1de5e685c8d
DavidH1982 <david.houghton1982@gmail.com>
2022-01-06 10:14:32 +0000

    first commit enjancement/add_mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs")
 const fs = require("fs")
-const { addMovie, listMovies, delMovies } = require("./utils/index")
+const { addMovie, delMovies, changeMovie } = require("./utils/index")
 
 const app = () => {
     try {
@@ -14,11 +14,24 @@ const app = () => {
                 if (process.argv[2] === 'add'){
                     addMovie(movieArray, {Title: yargs.argv.title, Actor: yargs.argv.actor});
                 } else if (process.argv[2] === 'list'){
-                    listMovies(movieArray);
-                } 
-                // else if (process.argv[2] === 'del'){
-                //     delMovies(movieArray, process.argv[3])
-                // }
+                    console.log(movieArray);
+                } else if (process.argv[2] === 'del'){
+                    delMovies(movieArray, process.argv[3])
+                    console.log(`${movieArray[process.argv[3]]} has been removed`)
+                    console.log(movieArray)
+                } else if (process.argv[2] === 'show'){
+                    if (process.argv[3] <= movieArray.length){
+                    console.log(movieArray[process.argv[3]-1])
+                    } else {
+                        console.log("There is not that many items in the list. Please try again")
+                    }
+                } else if (process.argv[2] === "update"){
+                    let newMovie = {Title: process.argv[4], Actor: process.argv[5]}
+                    changeMovie(movieArray, process.argv[3], newMovie)
+                    console.log(movieArray)
+                } else {
+                    console.log("Sorry, I did not recognise that command. Please try again.")
+                }
             } catch (error) {
                 console.log(error);
             }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,34 +1,38 @@
 const fs = require("fs");
 
+
 const addMovie = (movieArray, movieObj) => {
     try {
         movieArray.push(movieObj);
         const stringyObj = JSON.stringify(movieArray);
-        fs.writeFileSync('../storage.json', stringyObj, null, 2);
+        fs.writeFileSync('../storage.json', stringyObj);
     } catch (error) {
         console.log(error);        
     }
 }
 
-const listMovies = () => {
+const delMovies = (movieArray, movieObj) => {
     try {
-        console.log(JSON.parse(fs.readFileSync('../storage.json')));
+        movieArray.splice(movieObj, 1);
+        const stringyObj = JSON.stringify(movieArray);
+        fs.writeFileSync('../storage.json', stringyObj);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const changeMovie = (movieArray, index, newMovie) => {
+    try {
+        movieArray.splice(index, 1, newMovie)
+        const stringyObj = JSON.stringify(movieArray);
+        fs.writeFileSync('../storage.json', stringyObj);
     } catch (error) {
         console.log(error)
     }
 }
 
-// const delMovies = (movieArray, index) => {
-//     try {
-//         JSON.parse(fs.readFileSync('../storage.json'));
-//         movieArray.splice(index, 1);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 module.exports = {
     addMovie,
-    listMovies,
-    // delMovies,
+    delMovies,
+    changeMovie,
 }
