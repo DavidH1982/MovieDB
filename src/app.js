da60c102cf11cc7b095a37bdbdb78af3251937a9
DavidH1982 <david.houghton1982@gmail.com>
2022-01-07 10:06:45 +0000

    update with crud functions

--- src/app.js
+++ src/app.js
@@ -1,43 +1,60 @@
-const yargs = require("yargs")
-const fs = require("fs")
-const { addMovie, delMovies, changeMovie } = require("./utils/index")
+const yargs = require("yargs");
+const fs = require("fs");
+const {
+  addMovie,
+  findMovie,
+  delMovies,
+  changeMovie,
+  changeActor,
+} = require("./utils/index");
 
-const app = () => {
-    try {
-        let movieArray;
-            try {
-                movieArray = JSON.parse(fs.readFileSync('../storage.json'))   //parse = opposite of stringify
-            } catch (error) {
-                movieArray = [];
-            }
-            try {
-                if (process.argv[2] === 'add'){
-                    addMovie(movieArray, {Title: yargs.argv.title, Actor: yargs.argv.actor});
-                } else if (process.argv[2] === 'list'){
-                    console.log(movieArray);
-                } else if (process.argv[2] === 'del'){
-                    delMovies(movieArray, process.argv[3])
-                    console.log(`${movieArray[process.argv[3]]} has been removed`)
-                    console.log(movieArray)
-                } else if (process.argv[2] === 'show'){
-                    if (process.argv[3] <= movieArray.length){
-                    console.log(movieArray[process.argv[3]-1])
-                    } else {
-                        console.log("There is not that many items in the list. Please try again")
-                    }
-                } else if (process.argv[2] === "update"){
-                    let newMovie = {Title: process.argv[4], Actor: process.argv[5]}
-                    changeMovie(movieArray, process.argv[3], newMovie)
-                    console.log(movieArray)
-                } else {
-                    console.log("Sorry, I did not recognise that command. Please try again.")
-                }
-            } catch (error) {
-                console.log(error);
-            }
-    } catch (error) {
-        console.log(error);
+const connection = require("./db/connection");
+const { title } = require("process");
+
+const command = process.argv[2];
+const app = async (args) => {
+  // try {
+  // let movieArray;
+  //     try {
+  //         movieArray = JSON.parse(fs.readFileSync('../storage.json'))   //parse = opposite of stringify
+  //     } catch (error) {
+  //         movieArray = [];
+  //     }
+  // console.log(args.add)
+  try {
+    if (command === "add") {
+      const movieObj = { Title: args.title, Actor: args.actor };
+      await connection(addMovie, movieObj);
+
+      // addMovie(movieArray, {Title: yargs.argv.title, Actor: yargs.argv.actor});
+      // } else if (process.argv[2] === 'list'){
+      //     console.log(movieArray);
+    } else if (command === "del") {
+      const movieObj = { Title: process.argv[3] };
+      await connection(delMovies, movieObj);
+      console.log(`${movieObj.Title} has been removed`);
+    } else if (command === "find") {
+      const movieObj = { Title: process.argv[3] };
+      await connection(findMovie, movieObj);
+    } else if (command === "updateTitle") {
+      const movieObj = { Title: process.argv[3], Actor: process.argv[4] };
+      const newMovie = { Title: process.argv[5], Actor: movieObj.Actor };
+      await connection(changeMovie, movieObj);
+      console.log(`${movieObj.Title} has been replaced with ${newMovie.Title}`);
+    } else if (command === "updateActor") {
+      const movieObj = { Title: process.argv[4], Actor: process.argv[3] };
+      const newActor = { Title: movieObj.Title, Actor: process.argv[5] };
+      await connection(changeActor, movieObj);
+      console.log(`${movieObj.Actor} has been replaced with ${newActor.Actor}`);
+    } else {
+      console.log("Sorry, I did not recognise that command. Please try again.");
     }
+  } catch (error) {
+    console.log(error);
+  }
+  // } catch (error) {
+  //     console.log(error);
+  // }
 };
 
-app();
+app(yargs.argv);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,38 +1,89 @@
 const fs = require("fs");
 
+const addMovie = async (collection, movieObj) => {
+  try {
+    await collection.insertOne(movieObj);
+    console.log(`Successfully added ${movieObj.Title}.`);
+    // movieArray.push(movieObj);
+    // const stringyObj = JSON.stringify(movieArray);
+    // fs.writeFileSync('../storage.json', stringyObj);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const addMovie = (movieArray, movieObj) => {
-    try {
-        movieArray.push(movieObj);
-        const stringyObj = JSON.stringify(movieArray);
-        fs.writeFileSync('../storage.json', stringyObj);
-    } catch (error) {
-        console.log(error);        
+const findMovie = async (collection, movieObj) => {
+  try {
+    const movie = await collection.findOne(movieObj);
+    if (movie == null) {
+      console.log("There are no results for your search. Make sure spelling and capitalisation is correct");
+    } else {
+      console.log(movie);
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const delMovies = (movieArray, movieObj) => {
-    try {
-        movieArray.splice(movieObj, 1);
-        const stringyObj = JSON.stringify(movieArray);
-        fs.writeFileSync('../storage.json', stringyObj);
-    } catch (error) {
-        console.log(error);
-    }
-}
+// const findActor = async (collection, movieObj) => {
+//     try {
+//         const query = { actor: process.argv[3]}
+//         const options = {
+//             sort: {Title: 1},
+//             projection: { _id: 0, Title: 1, Actor: 0},
+//         };
+//         const cursor = collection.find(query, options);
+//         if ((await cursor.count()) === 0) {
+//             console.log("No results found")
+//         } else {
+//         await cursor.forEach(console.log());
+//         }
+//     }
+//         catch (error) {
+//         console.log(error);
+//     }
+// }
 
-const changeMovie = (movieArray, index, newMovie) => {
-    try {
-        movieArray.splice(index, 1, newMovie)
-        const stringyObj = JSON.stringify(movieArray);
-        fs.writeFileSync('../storage.json', stringyObj);
-    } catch (error) {
-        console.log(error)
-    }
-}
+const changeMovie = async (collection, movieObj) => {
+  try {
+    const newMovie = { Title: process.argv[5], Actor: movieObj.Actor };
+    await collection.replaceOne(movieObj, newMovie);
+    // movieArray.splice(index, 1, newMovie)
+    // const stringyObj = JSON.stringify(movieArray);
+    // fs.writeFileSync('../storage.json', stringyObj);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const changeActor = async (collection, movieObj) => {
+  try {
+    const newActor = { Actor: process.argv[5], Title: movieObj.Title };
+
+    await collection.replaceOne(movieObj, newActor);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const delMovies = async (collection, movieObj) => {
+  try {
+    await collection.deleteOne(movieObj);
+    console.log(
+      `Successfully removed ${movieObj.Title} from the Movie Database.`
+    );
+    // movieArray.splice(movieObj, 1);
+    // const stringyObj = JSON.stringify(movieArray);
+    // fs.writeFileSync('../storage.json', stringyObj);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    addMovie,
-    delMovies,
-    changeMovie,
-}
+  addMovie,
+  findMovie,
+  changeMovie,
+  changeActor,
+  delMovies,
+};
